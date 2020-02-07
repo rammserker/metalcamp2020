bcbff4a63ddf9736edf477c22b50a4ca3c3b4fbb
Maximiliano Fernández <mafernandez@miem.local>
2020-02-07 14:45:50 -0300

    Cambios en totest.sh para uglificar el JS

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,4 @@
-const version = '0.4',
+const version = '0.6',
     secciones = [],
     data = {},
     rutas = {
--- totest.sh
+++ totest.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 # DESTINATION="../cassetteuy.github.io/test/metalcamp2020/";
 DESTINATION="../carnivalfestpaysandu.github.io/";
+
+echo "Copiando archivos a ${DESTINATION}...";
 cp -u index.html $DESTINATION;
 cp -u favicon.png $DESTINATION;
-cp -u -r js ${DESTINATION};
+# cp -u -r js ${DESTINATION};
 cp -u -r css ${DESTINATION};
 cp -u -r img ${DESTINATION};
 cp -u -r fonts ${DESTINATION};
 cp -u -r data ${DESTINATION};
+echo "> LISTO!";
 
+# Compresión del js para producción
+echo "Comprimiendo JS...";
+uglifyjs js/app2.js --compress --mangle --ecma 8 -o ${DESTINATION}js/app2.js;
+echo "> LISTO!";
+
+echo "Agregando archivos para commit...";
 cd $DESTINATION;
-git add *;
+git add --all;
 cd -;
+echo "> LISTO!";
